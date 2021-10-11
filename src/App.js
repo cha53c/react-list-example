1211adb44bd744d3597c36daf32b98b89133409b
chrischase <chr15t0pher.cha53@gmail.com>
2021-10-11 19:27:36 +0100

    added EmployeeTable Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from "react";
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
+import Table from "react-bootstrap/Table";
 
 // import {EmployeeList} from "./EmployeeList";
 // import {EnterEmployee} from "./EnterEmployee";
@@ -15,7 +17,8 @@ function App() {
         console.log(employees);
     }
     const deleteEmployee = (indexToRemove) => {
-        console.log('deleting index', indexToRemove);;
+        console.log('deleting index', indexToRemove);
+        ;
         setEmployees(employees.filter((item, index) => index !== indexToRemove));
     }
     const EmployeeList = ({employees, deleteEmployee}) => {
@@ -29,16 +32,45 @@ function App() {
         }
         const renderedEmployees = employees.map((employee, index) => {
             return (
-                <li key={index}>
+                <ListGroupItem key={index}>
                     <EmployeeItem employee={employee} index={index}/>
-                </li>
+                </ListGroupItem>
             )
         })
 
         return (
-            <ol style={{paddingLeft: 0}}>
+            <ListGroup as="ol">
                 {renderedEmployees}
-            </ol>
+            </ListGroup>
+        )
+    }
+    const EmployeeTable = ({employees, deleteEmployee}) => {
+        const EmployeeRow = ({employee, index}) => {
+            return (<tr>
+                <td>{employee.name}</td>
+                <td>{employee.role}</td>
+                <td><Button className="delete" onClick={() => deleteEmployee(index)}>Delete</Button></td>
+            </tr>)
+        }
+        const renderedEmployees = employees.map((employee, index) => {
+            return (
+                <EmployeeRow employee={employee} index={index}/>
+            )
+        })
+
+        return (
+            <Table table table-sm>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                {renderedEmployees}
+                </tbody>
+            </Table>
         )
     }
     const EnterEmployee = ({addEmployee}) => {
@@ -64,11 +96,11 @@ function App() {
         return (<Form onSubmit={submitHandler}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="employee name" onChange={changeNameHandler} />
+                <Form.Control type="text" placeholder="employee name" onChange={changeNameHandler}/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Role</Form.Label>
-                <Form.Control type="text" placeholder="employee's role" onChange={changeRoleHandler} />
+                <Form.Control type="text" placeholder="employee's role" onChange={changeRoleHandler}/>
             </Form.Group>
             <Button variant="primary" type="submit">Add</Button>
         </Form>);
@@ -76,7 +108,8 @@ function App() {
     return (
         <div className="App">
             <EnterEmployee addEmployee={addEmployee}/>
-            <EmployeeList employees={employees} deleteEmployee={deleteEmployee}/>
+            {/*<EmployeeList employees={employees} deleteEmployee={deleteEmployee}/>*/}
+            <EmployeeTable employees={employees} deleteEmployee={deleteEmployee}/>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
